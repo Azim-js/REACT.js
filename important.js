202f1d0cdade4376207d5909fa23b8b3029de217
azim <az1234im@gmail.com>
2021-07-04 10:23:54 +0530

    strind${},rest,spread

--- important.js
+++ important.js
@@ -15,3 +15,57 @@ const arrTwo=arr.reduce(function(acc,val){
 },0);
 
 console.log(arrTwo);
+
+// string interpolation 
+
+const name = function(){
+    return "AZIM"
+}
+
+sentence= `Hi my name is ${name()}`;
+
+console.log(sentence);
+
+// object and array destructing 
+
+const wordArr=["hi","my","name","is","Azim"];
+const person={
+    name:"Azim",
+    hobby:"playing scooer",
+    job:"Intern",
+    age:'22'
+}
+
+function someName([,,,,lastEL]=[],{job}={}){
+    console.log(lastEL+"->"+job);
+}
+
+someName(wordArr,person);
+
+// rest and spread operators
+// rest
+// for object ... rest operator
+const {age,hobby,...newObj}=person;
+
+console.log(age,hobby,newObj);
+
+// for array ... rest operator
+const[firstEL,...rest]=wordArr;
+console.log(firstEL,rest);
+
+// spread
+// for object ... spread operator for merging
+
+const extendedPerson={
+    ...person,
+    nickname:"someword",
+    wow:"wow"
+
+}
+console.log(extendedPerson);
+
+// for array ... spread operator for merging
+
+const arr2=[16,14];
+const arr3=[...wordArr,...arr2];
+console.log(arr3);
