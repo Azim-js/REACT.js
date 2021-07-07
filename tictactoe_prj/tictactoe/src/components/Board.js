e396eb9d558ddf32543ee93f2ab9ad1fa9ebe5f5
azim <az1234im@gmail.com>
2021-07-07 19:39:57 +0530

    useState(the value,func())update the functionlatiy

--- tictactoe_prj/tictactoe/src/components/Board.js
+++ tictactoe_prj/tictactoe/src/components/Board.js
@@ -2,23 +2,45 @@ import React,{useState} from 'react'
 import Square from './Square'
 const Board = () => {
     const [board,setBoard]=useState(Array(9).fill(null));
-    console.log(board);
+    const [isXNext,setisXNext]=useState(false);
+    
+    // console.log(board);
+    const handleSquareClick=(position)=>{
+        // console.log(position);
+        if(board[position]){
+            return;
+        }
+        setBoard( (prev)=>{
+            return prev.map((square,pos)=>{
+                if(pos==position){
+                    return isXNext ? 'X':'O';
+                }
+                return square;
+            })
+        })
+        setisXNext(prev=>!prev);
+    }
+
+    const renderSquare=(position)=>{
+        return <Square value={board[position]} onClick={()=>handleSquareClick(position)}/>
+    }
+
     return (
         <div className="board">
             <div className="board-row">
-                <Square value={board[0]}/>
-                <Square value={board[1]}/>
-                <Square value={board[2]}/>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-                <Square value={board[3]}/>
-                <Square value={board[4]}/>
-                <Square value={board[5]}/>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-                <Square value={board[6]}/>
-                <Square value={board[7]}/>
-                <Square value={board[8]}/>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     )
--- tictactoe_prj/tictactoe/src/components/Square.js
+++ tictactoe_prj/tictactoe/src/components/Square.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
-const Square = ({value}) => {
+const Square = ({value,onClick}) => {
     // using{destructing method}
     // console.log(props);
     return (
-       <button type="button" className="square" onClick={()=>{
-           console.log(value);
-    }
-       }>{value}</button>
+       <button type="button" className="square" onClick={onClick}>{value}</button>
     )
 }
 
