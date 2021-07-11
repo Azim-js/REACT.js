e5f0b68125b946a113a0f78dd60304de35226177
azim <az1234im@gmail.com>
2021-07-11 14:54:02 +0530

    aing gasme history

--- tictactoe_prj/tictactoe/src/App.js
+++ tictactoe_prj/tictactoe/src/App.js
@@ -4,33 +4,40 @@ import {calculateWinner} from "./helper"
 import "./styles/root.scss"
 
 const App=()=>{
-  const [board,setBoard]=useState(Array(9).fill(null));
-    const [isXNext,setisXNext]=useState(false);
-    const winner=calculateWinner(board);
+  const [history,setHistory]=useState([{board:Array(9).fill(null),isXNext:true}]);
+  const [currentMove,SetCurrentMove]=useState(0);
+  
+  const current=history[currentMove];
+    // const [isXNext,setisXNext]=useState(false);
+    const winner=calculateWinner(current.board);
     console.log(winner);
-    console.log(board);
-    const message = winner ? `Winner is ${winner}` : ` Next Player is ${isXNext ? 'X':'O'} `;
+    // console.log(current.board);
+    console.log(history);
+    const message = winner ? `Winner is ${winner}` : ` Next Player is ${current.isXNext ? 'X':'O'} `;
     const handleSquareClick=(position)=>{
         // console.log(position);
-        if(board[position] || winner){
+        if(current.board[position] || winner){
             return;
         }
-        setBoard( (prev)=>{
-            return prev.map((square,pos)=>{
+        setHistory( (prev)=>{
+          const last=prev[prev.length-1];
+            const newBoard=last.board.map((square,pos)=>{
                 if(pos==position){
-                    return isXNext ? 'X':'O';
+                    return last.isXNext ? 'X':'O';
                 }
                 return square;
             })
+            return prev.concat({board:newBoard,isXNext:!last.isXNext})
         })
-        setisXNext(prev=>!prev);
+        SetCurrentMove(prev=>prev+1);
+        // setisXNext(prev=>!prev);
     }
   return(
   < >
   <div className="app">
     <h1>X-O GAME !</h1>
     <h2>{message}</h2>
-    <Board board={board} handleSquareClick={handleSquareClick}/>
+    <Board board={current.board} handleSquareClick={handleSquareClick}/>
     </div>
     
   </>);
