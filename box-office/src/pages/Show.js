1355a120f8c9796317679ed912eca9b7c1e6e570
azim <az1234im@gmail.com>
2021-08-12 23:45:46 +0530

    using {useEffect} to aoutmate the update fro url

--- box-office/src/pages/Show.js
+++ box-office/src/pages/Show.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React,{useEffect , useState} from 'react';
+import { useParams } from 'react-router-dom'
+import {apiGet} from '../misc/config'
+
 
 const Show = () => {
-    const params=useParams();
+    const {id}=useParams();
+    const [show,setShow]=useState(null);
     // eslint-disable-next-line
-    console.log("params",params)
+    console.log("params",{id})
+    useEffect(()=>{
+        apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`).then(result=>{
+            setShow(result);
+        })
+    },[id])
+// eslint-disable-next-line
+console.log("show",show)
+
+
     return (
         <div>
             This is Show Page
