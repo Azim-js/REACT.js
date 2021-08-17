a6d9d3a6854d0606f63908b3c2cbc56a10596740
azim <az1234im@gmail.com>
2021-08-17 13:05:22 +0530

    defined theme obj comop and created styled comp

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import {Switch,Route} from "react-router-dom"
+import { ThemeProvider } from 'styled-components' 
 import Show from './pages/Show'
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
 
+
 function App() {
+
+  const theme = {
+    mainColors: {
+      blue: '#2400ff',
+      gray: '#c6c6c6',
+      dark: '#353535',
+    }
+  };
+  
   return (
-   
+   <ThemeProvider theme={theme}>
     <Switch>
     <Route exact path="/">
       <Home />
@@ -23,7 +34,7 @@ function App() {
       </div>
     </Route>
     </Switch>
-    
+    </ThemeProvider>
   );
 }
 
