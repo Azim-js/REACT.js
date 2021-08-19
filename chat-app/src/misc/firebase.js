beb774775afc1f3ad4e60131c36d429351445aa3
azim <az1234im@gmail.com>
2021-08-19 20:15:47 +0530

    added firbase hosting configs

---
+++ chat-app/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config={
+    apiKey: "AIzaSyC1B8e_4bdI_LquUXbFi3iMvCIO5knfVus",
+    authDomain: "chit-chat-713cc.firebaseapp.com",
+    projectId: "chit-chat-713cc",
+    storageBucket: "chit-chat-713cc.appspot.com",
+    messagingSenderId: "777075645270",
+    appId: "1:777075645270:web:bd94ea846ee7f5f2b514f7"
+  };
+
+const  firebase.initializeApp(config);
