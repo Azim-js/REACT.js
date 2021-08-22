06df2e246adf61fa6e840cddf8f6cd1ca8edeee4
azim <az1234im@gmail.com>
2021-08-22 18:35:33 +0530

    manage user with profileContext;put real sub

--- chat-app/src/components/PrivateRoute.js
+++ chat-app/src/components/PrivateRoute.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router'
+import { Container, Loader } from 'rsuite';
 import { useProfile } from '../context/profile.context';
 
 const PrivateRoute = ({children, ...routeProps}) => {
     // eslint-disable-next-line
     console.log({...routeProps})
-    const profile=useProfile();
-    if(!profile){
+    const {profile,isLoading}=useProfile();
+
+    if(isLoading && !profile){
+        return <Container>
+            <Loader center vertical size="md" content="Loading" speed="slow"/>
+        </Container>
+    }
+
+    if(!profile && !isLoading){
         return <Redirect to ="/signin"/>
     }
     return (
--- chat-app/src/context/profile.context.js
+++ chat-app/src/context/profile.context.js
@@ -1,10 +1,59 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { auth, database } from '../misc/firebase';
 
 const ProfileContext=createContext();
 
 export const ProfileProvider=({children})=>{
-    const [profile]=useState(false);
-    return <ProfileContext.Provider value={profile}>{children}</ProfileContext.Provider>
+    const [profile,setProfile]=useState(null);
+    const [isLoading,setIsLoading]=useState(true);
+
+    useEffect(()=>{
+        let userRef;
+        const authUnsub=auth.onAuthStateChanged(authObj=>{
+            
+            // eslint-disable-next-line 
+            console.log(authObj)
+
+            
+            if(authObj){
+                userRef=database.ref(`/profiles/${authObj.uid}`)
+                // real time subscription 
+                userRef.on('value',(snap)=>{
+                // eslint-disable-next-line 
+                console.log(snap.val())
+                const {name,createdAt}=snap.val()
+
+                const data={
+                    name,
+                    createdAt,
+                    uid:authObj.uid,
+                    email:authObj.email
+                }
+                setProfile(data);
+                setIsLoading(false);
+
+            })
+
+                
+            }
+            else{
+                if(userRef){
+                    userRef.off();
+                }
+                   setProfile(null) ;
+                   setIsLoading(false);
+            }
+
+        })
+        return()=>{
+            authUnsub();
+            if(userRef){
+                userRef.off();
+            }
+        }
+    },[])
+
+    return <ProfileContext.Provider value={{isLoading,profile}}>{children}</ProfileContext.Provider>
 }
 
 export const useProfile=()=>useContext(ProfileContext)
