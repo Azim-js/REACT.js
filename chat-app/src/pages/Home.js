21fa0773f7ed94df8231412b28e44aea3ceb3c0a
azim <az1234im@gmail.com>
2021-08-22 21:11:50 +0530

    started building the side bar component

--- chat-app/src/pages/Home.js
+++ chat-app/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Col, Grid, Row, } from 'rsuite'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
     return (
-        <div>
-            this is home page
-        </div>
+        
+        <Grid fluid className="h-100">
+            <Row>
+                <Col xs={24} md={8}>
+                    <Sidebar />
+                </Col>
+            </Row>
+
+        </Grid>
     )
 }
 
