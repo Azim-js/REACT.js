f516f411d8106c2f02afddd1163a445867f7a4e5
azim <az1234im@gmail.com>
2021-08-29 18:58:00 +0530

    linked faceboogk and google to one acc prov block

--- chat-app/src/components/dashboard/ProviderBlock.js
+++ chat-app/src/components/dashboard/ProviderBlock.js
@@ -1,11 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert, Button, Icon, Tag } from 'rsuite'
+import firebase from 'firebase'
 import { auth } from '../../misc/firebase'
 
 const ProviderBlock = () => {
+    // eslint-disable-next-line no-console
+    console.log(auth.currentUser)
+    
+    const [isConnected,SetIsConnected]=useState({
+        'google.com':auth.currentUser.providerData.some((data)=>data.providerId==='google.com'),
+        'facebook.com':auth.currentUser.providerData.some((data)=>data.providerId==='facebook.com')
+    })
+
+    const updateIsConnected=(providerId,value)=>{
+        SetIsConnected(p=>{
+            return{
+                ...p,
+                [providerId]:value,
+            }
+        })
+    }
+
+    const unlink=async (providerId)=>{
+        try{
+
+            if(auth.currentUser.providerData.length ===1){
+                throw new  Error(`you Cannot disconnect from ${providerId}`)
+            }
+
+        await auth.currentUser.unlink(providerId);
+        updateIsConnected(providerId,false);
+        Alert.info(`Disconnected from ${providerId}`,4000)
+
+        }catch(err){
+            Alert.error(err.message,4000);
+        }
+    }
+
+    const unLinkFacebook=()=>{
+        unlink('facebook.com');
+    }
+    const unLinkGoogle=()=>{
+        unlink('google.com')
+    }
+
+    const link=async (provider)=>{
+        // athunticationg with firbase athuentication 
+        try{
+            await auth.currentUser.linkWithPopup(provider)
+            Alert.info(`Linked to ${provider.providerId}`,4000)
+            updateIsConnected(provider.providerId,true);
+        }catch(err){
+            Alert.error(err.message,4000)
+        }
+
+    }
+
+    const linkFacebook=()=>{
+        link (new firebase.auth.FacebookAuthProvider())
+    }
+    const linkGoogle=()=>{
+        link (new firebase.auth.FacebookAuthProvider())
+    }
+
     return (
-        console.log(auth.currentUser)
+        
         <div>
-            hello
+            {isConnected['google.com'] && 
+            <Tag color="green" closable onClose={unLinkGoogle}>
+               <Icon icon="google"/> Connected
+           </Tag>
+           }
+           
+           {isConnected['facebook.com'] && 
+           <Tag color="blue" closable onClose={unLinkFacebook}>
+           <Icon icon="facebook"/> Connected
+       </Tag>
+           }
+           
+           
+        <div className="mt-2">
+        { !isConnected['google.com'] && 
+        <Button block color="green" onClick={linkGoogle}>
+            <Icon icon="google" /> Link to Google
+        </Button>
+           
+        }
+            
+            { !isConnected['facebook.com'] && 
+            <Button block color="blue" onClick={linkFacebook}>
+                <Icon icon="facebook" /> Connected to Facebook
+            </Button>
+            }
+
+            
+
+        </div>
         </div>
     )
 }
